dffd69baa56de707dc91f88c6b8ca7b17c0cd4c9
laybrid <664531349@qq.com>
2025-03-13 14:52:06 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
-  <nav class="fixed top-0 right-5">
-    <button @click="changeMode" class="hover:text-[#0d9488]">
-      <SunIcon class="size-6" v-show="modeState"></SunIcon>
-      <MoonIcon class="size-6" v-show="!modeState"></MoonIcon>
-    </button>
-    <a href="#" class="hover:text-[#0d9488]">GitHub</a>
-  </nav>
+  <p class="bg-color-background text-sms">123</p>
+  <p class="bg-color-background-d text-sm">123</p>
+  <p class="bg-color-highlight-background text-md">123</p>
+  <p class="bg-color-dialog-background text-mdx">123</p>
+  <p class="bg-color-theme text-lg">123</p>
+  <p class="bg-color-theme-d text-lgx">123</p>
+  <p class="bg-color-sub-theme">123</p>
+  <p class="bg-color-text">123</p>
+  <p class="text-color-text-d">123</p>
+  <p class="text-color-text-l">123</p>
+  <p class="text-color-text-ll">123</p>
   <router-view />
 </template>
 
 <script setup lang="ts">
-import { useDarkMode } from './assets/utils/useDarkMode'
-import { MoonIcon, SunIcon,ArrowDownCircleIcon } from '@heroicons/vue/24/outline'
-const { modeState, changeMode } = useDarkMode()
 </script>
 
 <style>
